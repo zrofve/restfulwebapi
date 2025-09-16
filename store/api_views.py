1d45b32ed27b33815fa7f8542b9fdd5faecec375
zrofve <zrofvedev@gmail.com>
2025-09-16 20:50:02 +0800

    Adding filter by ID

--- store/api_views.py
+++ store/api_views.py
@@ -1,8 +1,11 @@
 from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+
 from store.serializers import ProductSerializer
 from store.models import Product
 
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('id',)
