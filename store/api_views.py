61a3db73ba7f3b3e275d202b685823c273be2bea
zrofve <zrofvedev@gmail.com>
2025-09-21 20:08:59 +0800

    Add ProductDestroy - DestroyAPIView

--- store/api_views.py
+++ store/api_views.py
@@ -1,5 +1,5 @@
 from rest_framework.exceptions import ValidationError
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
@@ -12,6 +12,7 @@ class ProductPagination(LimitOffsetPagination):
     default_limit = 10
     max_limit = 100
 
+# view product list
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -34,6 +35,7 @@ class ProductList(ListAPIView):
             )
         return queryset
 
+# create product
 class ProductCreate(CreateAPIView):
     serializer_class = ProductSerializer
 
@@ -45,3 +47,16 @@ class ProductCreate(CreateAPIView):
         except ValueError:
             raise ValidationError({'price:' 'A valid number is required'})
         return super().create(request, *args, **kwargs)
+
+# delete/destroy product
+class ProductDestroy(DestroyAPIView):
+    queryset = Product.objects.all()
+    lookup_field = 'id'
+    
+    def delete(self, request, *args, **kwargs):
+        product_id = request.data.get('id')
+        response = super().delete(request, *args, **kwargs)
+        if response.status_code == 204:
+            from django.core.cache import cache
+            cache.delete('productr_data_{}'.format(product_id))
+        return response
