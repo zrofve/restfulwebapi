1a4017f1fa6405ec47ff7de0c5006f4c909f422a
zrofve <zrofvedev@gmail.com>
2025-09-24 23:55:56 +0800

    Handling image uploads in a unit test

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,5 @@
+import os.path
+from django.conf import settings
 from rest_framework.test import APITestCase
 
 from store.models import Product
@@ -61,3 +63,29 @@ class ProductUpdateTestCase(APITestCase):
         )
         updated = Product.objects.get(id=product.id)
         self.assertEqual(updated.name,'New Product')
+
+    def test_upload_product_photo(self):
+        product = Product.objects.first()
+        original_photo = product.photo
+        photo_path = os.path.join(
+            settings.MEDIA_ROOT , 'products', 'vitamin-iron.jpg',
+        )
+        with open(photo_path, 'rb') as photo_data:
+            response = self.client.patch(
+                '/api/v1/products/{}/'.format(product.id),
+                {'photo': photo_data},
+                format='multipart',
+            )
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data['photo'], original_photo)
+
+        try:
+            updated = Product.objects.get(id=product.id)
+            expected_photo = os.path.join(
+                settings.MEDIA_ROOT, 'products', 'vitamin-iron',
+            )
+            self.assertTrue(
+                updated.photo.path.startswith(expected_photo)
+            )
+        finally:
+            os.remove(updated.photo.path)
